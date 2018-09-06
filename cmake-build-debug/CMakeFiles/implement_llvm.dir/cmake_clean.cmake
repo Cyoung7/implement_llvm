744e66826de2d94baa42c8a1fbf1ab76466c7e4f
Cyoung7 <1594012301@qq.com>
2018-09-06 23:06:40 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/implement_llvm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/implement_llvm.dir/main.cpp.o"
+  "implement_llvm.pdb"
+  "implement_llvm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/implement_llvm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
